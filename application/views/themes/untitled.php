d27687fae676a8a8dd5eb5854bbddf400c218d0b
KennyKur <34298847+KennyKur@users.noreply.github.com>
2019-10-27 23:40:23 +0800

    Initial commit

---
+++ application/views/themes/untitled.php
@@ -0,0 +1,83 @@
+.ftitle {
+    margin-left: 10px !important;
+    line-height: 30px !important;
+    font-weight: normal !important;
+    font-size: 20px !important;
+    height: 30px !important;
+}
+
+.mDiv {
+    background: #fff !important;
+    border-color: rgb(222, 226, 230) !important;
+    font-family: "Source Sans Pro",-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol" !important;
+}
+.text-left.field-sorting {
+    font-weight: 600 !important;
+    font-size: 16px !important;
+    font-family: "Source Sans Pro",-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol" !important;
+}
+
+thead .text-right {
+    font-weight: 600 !important;
+    font-size: 16px !important;
+    font-family: "Source Sans Pro",-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol" !important;
+}
+
+th {
+    border-color: #dee2e6 !important;
+}
+
+tr.hDiv {
+    border-color: #dee2e6 !important;
+}
+
+th .text-left {
+    background: white;
+    color: #333;
+}
+
+th {
+    border-color: #dee2e6 !important;
+}
+
+.text-left {
+    border-color: #dee2e6 !important;
+}
+
+.text-left.field-sorting {
+    border-bottom: 1px solid #eceaea;
+}
+
+th > div {
+    background: white;
+}
+.tDiv {
+    background: #F5F5F5 !important;
+    border-color: #e6e6e6;
+}
+.pDiv {
+    background: #F5F5F5 !important;
+    border-color: #e6e6e6;
+}
+.ftitle {
+    border-top: 1px solid rgb(222, 226, 230) !important;
+    margin-left: 0px !important;
+    padding-left: 15px !important;
+    border-bottom: 1px solid rgb(222, 226, 230) !important;
+}
+.ptogtitle {
+    
+    margin-right: 10px;
+    margin-top: 10px;
+}
+table#flex1 td {
+    height: 35px;
+}
+
+table#flex1 {
+    margin-bottom: 0px;
+}
+.flexigrid div.tDiv
+{
+	border-top: none !important;
+}
